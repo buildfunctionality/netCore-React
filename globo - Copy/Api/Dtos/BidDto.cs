3383c7fcb7997d42f7e0b96efb73b38e6401f0e5
Alejandro Romero <aromero@alorahealth.com>
2025-03-13 14:29:02 -0300

    remove folder

--- globo - Copy/Api/Dtos/BidDto.cs
+++
@@ -1,5 +0,0 @@
-
-
-using System.ComponentModel.DataAnnotations;
-
-public record BidDto(int Id,int HouseId, [property: Required] string Bidder, int Amount);
--- globo - Copy/Api/Dtos/HouseDetailDto.cs
+++
@@ -1,5 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public record HouseDetailDto(int Id,
-[property: Required] string? Address,[property: Required] string? Country,int Price,
-string? Description, string? Photo);
